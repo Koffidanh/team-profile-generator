f5b05317013446e0dc026da65009266caacf3983
KoffiDanhounsrou <koffi.danh@gmail.com>
2021-02-19 17:29:51 -0700

    some index code working still need work

--- index.js
+++ index.js
@@ -118,146 +118,131 @@ function init() {
                                 default: true,
                             },
                         ]).then((answers) => {
-                            if (answers.moreTeam === true) {
-                                inquirer.prompt([
-                                    {
-                                        type: 'list',
-                                        name: 'moreRole',
-                                        message: 'Choose the role of the next team member!',
-                                        choices: ['Engineer', 'Intern'],
-                                    },
-                                    {
-                                        type: 'input',
-                                        name: 'moreEngineerName',
-                                        message: 'Enter the engineer name!',
-                                    },
-                                    {
-                                        type: 'input',
-                                        name: 'moreEngineerId',
-                                        message: 'Enter the engineer ID!',
-                                    },
-                                    {
-                                        type: 'input',
-                                        name: 'moreEngineerEmail',
-                                        message: 'Enter the engineer email!',
-                                    },
-                                    {
-                                        type: 'input',
-                                        name: 'moreEngineerGitHub',
-                                        message: 'Enter the engineer GitHub username!',
-                                    },
-                                    {
-                                        type: 'confirm',
-                                        name: 'moreEnterIntern',
-                                        message: 'Would you like to add an intern?',
-                                        choices: ['Yes', 'No'],
-                                        default: true,
-                                    },
-                                ]).then((answers) => {
-                                    if (answers.moreEnterIntern === true) {
-                                        inquirer.prompt([
-                                            {
-                                                type: 'input',
-                                                name: 'moreInternName',
-                                                message: 'Enter the Intern name!',
-                                            },
-                                            {
-                                                type: 'input',
-                                                name: 'moreInternId',
-                                                message: 'Enter the intern ID!',
-                                            },
-                                            {
-                                                type: 'input',
-                                                name: 'moreInternEmail',
-                                                message: 'Enter the intern email!',
-                                            },
-                                            {
-                                                type: 'input',
-                                                name: 'moreInternSchool',
-                                                message: 'Enter the intern school name!',
-                                            },
-                                            {
-                                                type: 'confirm',
-                                                name: 'moreTeam',
-                                                message: 'Would you like to add more team member?',
-                                                choices: ['Yes', 'No'],
-                                                default: true,
+                                if (answers.moreTeam === true) {
+                                    inquirer.prompt([
+                                        {
+                                            type: 'list',
+                                            name: 'moreRole',
+                                            message: 'Choose the role of the next team member!',
+                                            choices: ['Engineer', 'Intern'],
+                                        },
+                                        {
+                                            type: 'input',
+                                            name: 'moreEngineerName',
+                                            message: 'Enter the engineer name!',
+                                        },
+                                        {
+                                            type: 'input',
+                                            name: 'moreEngineerId',
+                                            message: 'Enter the engineer ID!',
+                                        },
+                                        {
+                                            type: 'input',
+                                            name: 'moreEngineerEmail',
+                                            message: 'Enter the engineer email!',
+                                        },
+                                        {
+                                            type: 'input',
+                                            name: 'moreEngineerGitHub',
+                                            message: 'Enter the engineer GitHub username!',
+                                        },
+                                        {
+                                            type: 'confirm',
+                                            name: 'moreEnterIntern',
+                                            message: 'Would you like to add an intern?',
+                                            choices: ['Yes', 'No'],
+                                            default: true,
+                                        },
+                                    ]).then((answers) => {
+                                            if (answers.moreEnterIntern === true) {
+                                                inquirer.prompt([
+                                                    {
+                                                        type: 'input',
+                                                        name: 'moreInternName',
+                                                        message: 'Enter the Intern name!',
+                                                    },
+                                                    {
+                                                        type: 'input',
+                                                        name: 'moreInternId',
+                                                        message: 'Enter the intern ID!',
+                                                    },
+                                                    {
+                                                        type: 'input',
+                                                        name: 'moreInternEmail',
+                                                        message: 'Enter the intern email!',
+                                                    },
+                                                    {
+                                                        type: 'input',
+                                                        name: 'moreInternSchool',
+                                                        message: 'Enter the intern school name!',
+                                                    },
+                                                    {
+                                                        type: 'confirm',
+                                                        name: 'moreTeam',
+                                                        message: 'Would you like to add more team member?',
+                                                        choices: ['Yes', 'No'],
+                                                        default: true,
 
+                                                    }
+
+                                                ])
                                             }
+                                        }
+                                        )
+                                   
 
-                                        ])
-                                    }
+
+                                }
+
+                                else {
+                                    inquirer.prompt([
+                                        {
+                                            type: 'input',
+                                            name: 'internName',
+                                            message: 'Enter the Intern name!',
+                                        },
+                                        {
+                                            type: 'input',
+                                            name: 'internId',
+                                            message: 'Enter the intern ID!',
+                                        },
+                                        {
+                                            type: 'input',
+                                            name: 'internEmail',
+                                            message: 'Enter the intern email!',
+                                        },
+                                        {
+                                            type: 'input',
+                                            name: 'internSchool',
+                                            message: 'Enter the intern school name!',
+                                        },
+                                        {
+                                            type: 'list',
+                                            name: 'moreTeam',
+                                            message: 'Would you like to add more team member?',
+                                            choices: ['Yes', 'No'],
+                                        },
+
+                                    ])
+                                }
+                            })
+                                .catch((err) => {
+                                    throw new Error(err);
                                 })
                                 .then((answers) => {
                                     console.log('\nUser input:');
-                                    console.log(JSON.stringify(answers, null, '  '));
+                                    console.log(JSON.stringify(data, null, '  '));
 
                                     //writeFileAsync('README.md', generateMarkdown((data)))
 
                                 })
-                                .catch((err) => {
-                                    throw new Error(err);
-                                })
-                            }
-                            else {
-                                inquirer.prompt([
-                                    {
-                                        type: 'input',
-                                        name: 'internName',
-                                        message: 'Enter the Intern name!',
-                                    },
-                                    {
-                                        type: 'input',
-                                        name: 'internId',
-                                        message: 'Enter the intern ID!',
-                                    },
-                                    {
-                                        type: 'input',
-                                        name: 'internEmail',
-                                        message: 'Enter the intern email!',
-                                    },
-                                    {
-                                        type: 'input',
-                                        name: 'internSchool',
-                                        message: 'Enter the intern school name!',
-                                    },
-                                    {
-                                        type: 'list',
-                                        name: 'moreTeam',
-                                        message: 'Would you like to add more team member?',
-                                        choices: ['Yes', 'No'],
-                                    },
-
-                                ])
-                                    .then((answers) => {
-                                        console.log('\nUser input:');
-                                        console.log(JSON.stringify(answers, null, '  '));
-
-                                        //writeFileAsync('README.md', generateMarkdown((data)))
-
-                                    })
-                                    .catch((err) => {
-                                        throw new Error(err);
-                                    })
-                            }
-                        })
-
+                       
                     }
                 })
 
-
             }
         })
-        .catch((err) => {
-            throw new Error(err);
-        })
-        .then((answers) => {
-            console.log('\nUser input:');
-            console.log(JSON.stringify(answers, null, '  '));
 
-            //writeFileAsync('README.md', generateMarkdown((data)))
-
-        })
 }
 //Calling the init function
 init();
